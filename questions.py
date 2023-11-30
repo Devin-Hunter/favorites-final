42c054d550af3ce79dea8c7d34d104de7cecaaa7
Devin-Matherne <devinmatherne@ymail.com>
2023-11-30 13:11:42 -0700

    answered all questions

--- questions.py
+++ questions.py
@@ -1,26 +1,28 @@
 # Welcome to the final Git exercise.
 # We're in a Python file again, but it looks like
-# we've been beaten to the punch! Please 
+# we've been beaten to the punch! Please
 # follow the instructions in Learn to get the repository
 # into a state where you can fill in your own answers!
 
 """
-!!!!! 
+!!!!!
   DO NOT MODIFY ANY OF THE CODE BELOW THIS LINE
 !!!!!
 """
 
+''' This is my template now !!!!!'''
+
 first_question = "What is your favorite color?"
-first_question_answer = ""
+first_question_answer = "lime green and blue"
 
 second_question = "What is your favorite food?"
-second_question_answer = ""
+second_question_answer = "anything my husband makes"
 
 third_question = "Who is your favorite fictional character? ex. Mickey Mouse, Bugs Bunny"
-third_question_answer = ""
+third_question_answer = "toph from avatar"
 
 fourth_question = "What is your favorite animal?"
-fourth_question_answer = ""
+fourth_question_answer = "racoons and dogs"
 
 fifth_question = "What is your favorite programming language? (Hint: You can always say Python!!)"
-fifth_question_answer = ""
+fifth_question_answer = "python so far!"
